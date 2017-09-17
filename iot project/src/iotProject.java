795341fc4be2a43259cf1ee83aaca723e97754a0
user <user@user-PC>
2017-09-17 23:34:02 +0900

    1

--- iot project/src/iotProject.java
+++ iot project/src/iotProject.java
@@ -17,7 +17,7 @@ public class iotProject {
 	
 	public static void main(String[] args) {
 		System.out.println("iot 세미나 2조입니다.");
-		System.out.println("20122994 한성진입니다.");
+		System.out.println(" 20122994 한성진입니다.");
 		System.out.println("20113148 조현태입니다.");
 		System.out.println("20133046 안필규입니다.");
 		
