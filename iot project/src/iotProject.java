686e171db0f4bc965f107b81e7a1cbe48428f101
ain53 <ain53@DESKTOP-AA50M6A>
2017-09-17 22:02:34 +0900

    100까지 카운트

--- iot project/src/iotProject.java
+++ iot project/src/iotProject.java
@@ -1,9 +1,25 @@
+import java.io.*;
+
+
 
 public class iotProject {
-
+	
+	public static void COUNT(){
+		int i = 0;
+		
+		System.out.println("20122948 박종국이 만든 100 까찌 COUNT");
+		while(i<100){
+			i++;
+			System.out.println(i);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("iot 세미나 2조입니다.");
 		System.out.println("20122994 한성진입니다.");
+		
+		COUNT();
 	}
 
 }
