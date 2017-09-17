403a25c71c09a8dc482f637bfed0c22b9ba082b6
PC <PC@DESKTOP-6NB9TUC>
2017-09-17 22:30:20 +0900

    추가

--- iot project/src/iotProject.java
+++ iot project/src/iotProject.java
@@ -18,6 +18,7 @@ public class iotProject {
 	public static void main(String[] args) {
 		System.out.println("iot 세미나 2조입니다.");
 		System.out.println("20122994 한성진입니다.");
+		System.out.println("20113148 조현태입니다.");
 		
 		COUNT();
 	}
